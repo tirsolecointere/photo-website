00f24c17311f6e8168995a24e7437bd4a52431d9
Tirso Lecointere <tirsolecointere@gmail.com>
2022-06-27 23:36:23 -0400

    delete commented old layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,6 @@
                 <a href="javascript:void(0)" class="block lowercase py-2 px-4 hover:underline hover:text-slate-800 text-slate-500">Other</a>
             </nav>
 
-            {{-- <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-6">
-                @foreach ($images as $image)
-                <div class="w-full aspect-[3/4]">
-                    <img src="{{ $image->url_md }}" alt="" class="w-full h-full object-cover">
-                </div>
-                @endforeach
-            </div> --}}
             <div class="masonry-grid">
                 @foreach ($images as $image)
                 <div class="masonry-grid__item float-left p-1 md:p-3 w-1/2 md:w-1/3 lg:w-1/4">
