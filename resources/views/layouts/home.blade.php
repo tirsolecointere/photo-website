fcc587ed1febad67333edc3977aab2be48585c93
Tirso Lecointere <tirsolecointere@gmail.com>
2022-11-16 11:52:09 -0400

    add google analytics script

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7CWNXW65QF"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-7CWNXW65QF');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
